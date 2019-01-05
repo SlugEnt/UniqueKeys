c2a17c20cf6f2a080c011094a41010a7d5d12c42
Scott Herrmann Personal <daborg69@gmail.com>
2019-01-05 11:52:32 -0500

    Initial UniqueKeys project and package

---
+++ UniqueKeys/UniqueKeys.cs
@@ -0,0 +1,68 @@
+﻿/*
+ * Copyright 2018 Scott Herrmann
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SlugEnt
+{
+	/// <summary>
+	/// Class will generate a short unique key value that is guaranteed to be unique across a given instance of the class.
+	/// Keys can be prefixed with a string upon each subsequent call.
+	/// </summary>
+	public class UniqueKeys
+	{
+		private int keyIncrementer = 0;
+		private object keyIncrLock = new object();
+		private string stGuid;
+
+		public UniqueKeys() {
+			DateTime d = DateTime.Now;
+			stGuid = TimeGuid.ConvertTimeToChar(d);
+		}
+
+
+		/// <summary>
+		/// Creates a small random key based upon the current time (H:M:S).
+		/// </summary>
+		/// <param name="prefix"></param>
+		/// <returns></returns>
+		public string GetKey(string prefix = "Key") {
+			string val = prefix + stGuid + IncrementKey();
+			return val;
+		}
+
+
+		public string RefreshKey(string prefix = "Key") {
+			DateTime d = DateTime.Now;
+			stGuid = TimeGuid.ConvertTimeToChar(d);
+			return GetKey(prefix);
+		}
+
+
+		private string IncrementKey() {
+			string val;
+			lock (keyIncrLock) {
+				keyIncrementer++;
+				val = keyIncrementer.ToString();
+			}
+			return val;
+		}
+	}
+}
---
+++ UniqueKeys_Test/TestUniqueKeys.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using NUnit.Framework;
+using SlugEnt;
+
+
+namespace SlugEnt.UnitTest
+{
+	[Parallelizable]
+	public class TestUniqueKeys
+	{
+		// Validates that the default prefix for keys is Key
+		[Test]
+		public void DefaultKeyPrefix_IsCorrect() {
+			UniqueKeys uniqueKey = new UniqueKeys();
+			string key = uniqueKey.GetKey();
+			string prefix = key.Substring(0, 3);
+			Assert.AreEqual(3, prefix.Length, "A1: String prefix was expected to be 3 characters long.");
+			Assert.AreEqual("Key", prefix, "A2: Default prefix is expected to be Key");
+		}
+
+
+
+		// Validates the key is generated as expected.
+		[Test]
+		public void KeyValueIsAsExpected_Success() {
+			string defaultPrefix = "Key";
+
+			// We need to simulate the Timeguid logic to derive at the initial key value.
+			DateTime d = DateTime.Now;
+			string stGuid = TimeGuid.ConvertTimeToChar(d);
+
+
+			UniqueKeys uniqueKey = new UniqueKeys();
+			string key = uniqueKey.GetKey();
+			string expKey = defaultPrefix + stGuid + "1";
+			Assert.AreEqual(expKey, key, "A1:  Expected initial key value was not correct. Note:  On Failures run a second time.  There is a very slight chance that due to a second rollover the values could be different.  Expected: " + expKey + "   Actual: " + key);
+			TestContext.WriteLine("Unique Key Value:    {0}", key);
+			TestContext.WriteLine("Expected Key Value:  {0}", expKey);
+		}
+
+
+
+		// Validates that the first key has a value of 1 and each subsequent call is increasing by 1.
+		public void KeyNumberShouldIncrement_EachCall_Success() {
+			string defaultPrefix = "Key";
+
+			// We need to simulate the Timeguid logic to derive at the initial key value.
+			DateTime d = DateTime.Now;
+			string stGuid = TimeGuid.ConvertTimeToChar(d);
+
+
+			UniqueKeys uniqueKey = new UniqueKeys();
+			string key = uniqueKey.GetKey();
+			string expKey = defaultPrefix + stGuid + "1";
+			Assert.AreEqual(expKey, key, "A1:  Expected initial key value was not correct. Note:  On Failures run a second time.  There is a very slight chance that due to a second rollover the values could be different.  Expected: " + expKey + "   Actual: " + key);
+
+
+			// Now generate another key:
+			string key2 = uniqueKey.GetKey();
+			string expKey2 = defaultPrefix + stGuid + "2";
+			Assert.AreEqual(expKey2, key2, "A2:  Expected initial key value was not correct. Note:  On Failures run a second time.  There is a very slight chance that due to a second rollover the values could be different.  Expected: " + expKey + "   Actual: " + key);
+		}
+
+
+		// Validate that refresh key generates a new time value for the unique key.
+		[Test]
+		public void RefreshKeyWorks() {
+			string defaultPrefix = "Key";
+
+			// We need to simulate the Timeguid logic to derive at the initial key value.
+			DateTime d = DateTime.Now;
+			string stGuid = TimeGuid.ConvertTimeToChar(d);
+
+
+			UniqueKeys uniqueKey = new UniqueKeys();
+			string key = uniqueKey.GetKey();
+			string expKey = defaultPrefix + stGuid + "1";
+			Assert.AreEqual(expKey, key, "A1:  Expected initial key value was not correct. Note:  On Failures run a second time.  There is a very slight chance that due to a second rollover the values could be different.  Expected: " + expKey + "   Actual: " + key);
+
+
+			// Now get a new key.  Note the TimeGuid portion should be updated and the increment should be updated.
+			Thread.Sleep(2000);
+
+			string key2 = uniqueKey.RefreshKey();
+			DateTime d2 = DateTime.Now;
+			string stGuid2 = TimeGuid.ConvertTimeToChar(d2);
+			string expKey2 = defaultPrefix + stGuid2 + "2";
+			Assert.AreEqual(expKey2, key2, "A2: Expected key after Refresh was not equal to the value returned by UniqueKeys.  Exp: " + expKey2 + "   Actual:  " + key2);
+		}
+
+
+
+		// Validate GetKey returns a key with the custom prefix.
+		[Test]
+		public void GetKeyWithUniquePrefix_Success() {
+			string defaultPrefix = "unique";
+
+			// We need to simulate the Timeguid logic to derive at the initial key value.
+			DateTime d = DateTime.Now;
+			string stGuid = TimeGuid.ConvertTimeToChar(d);
+
+
+			UniqueKeys uniqueKey = new UniqueKeys();
+			string key = uniqueKey.GetKey(defaultPrefix);
+			string expKey = defaultPrefix + stGuid + "1";
+			Assert.AreEqual(expKey, key, "A1:  Expected initial key value was not correct. Note:  On Failures run a second time.  There is a very slight chance that due to a second rollover the values could be different.  Expected: " + expKey + "   Actual: " + key);
+
+			TestContext.WriteLine("Unique Key Value:    {0}", key);
+			TestContext.WriteLine("Expected Key Value:  {0}", expKey);
+		}
+
+
+		// Validate RefreshKey returns a key with the custom prefix.
+		[Test]
+		public void RefreshKeyWithUniquePrefix_Success() {
+			string defaultPrefix = "unique";
+
+			// We need to simulate the Timeguid logic to derive at the initial key value.
+			DateTime d = DateTime.Now;
+			string stGuid = TimeGuid.ConvertTimeToChar(d);
+
+
+			UniqueKeys uniqueKey = new UniqueKeys();
+			string key = uniqueKey.GetKey(defaultPrefix);
+			string expKey = defaultPrefix + stGuid + "1";
+			Assert.AreEqual(expKey, key, "A1:  Expected initial key value was not correct. Note:  On Failures run a second time.  There is a very slight chance that due to a second rollover the values could be different.  Expected: " + expKey + "   Actual: " + key);
+
+
+			// Now get a new key.  Note the TimeGuid portion should be updated and the increment should be updated.
+			Thread.Sleep(2000);
+
+			string key2 = uniqueKey.RefreshKey(defaultPrefix);
+			DateTime d2 = DateTime.Now;
+			string stGuid2 = TimeGuid.ConvertTimeToChar(d2);
+			string expKey2 = defaultPrefix + stGuid2 + "2";
+			Assert.AreEqual(expKey2, key2, "A2: Expected key after Refresh was not equal to the value returned by UniqueKeys.  Exp: " + expKey2 + "   Actual:  " + key2);
+
+
+			TestContext.WriteLine("Unique Key Value:    {0}", key2);
+			TestContext.WriteLine("Expected Key Value:  {0}", expKey2);
+		}
+	}
+}
